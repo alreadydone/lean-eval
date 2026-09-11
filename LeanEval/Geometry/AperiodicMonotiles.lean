1641efd6dd652b9008e36d1fdfb44674527552b8
Junyan Xu <junyanxu.math@gmail.com>
2026-09-12 01:53:50 +0800

    improvements

--- LeanEval/Geometry/AperiodicMonotiles.lean
+++ LeanEval/Geometry/AperiodicMonotiles.lean
@@ -4,7 +4,7 @@ import EvalTools.Markers
 /-!
 # Aperiodic monotiles
 
-References:
+## References
 
 David Smith, Joseph Samuel Myers, Craig S. Kaplan, Chaim Goodman-Strauss.
 An aperiodic monotile, https://arxiv.org/abs/2303.10798
@@ -27,11 +27,11 @@ structure TilingBy (T X : Type*) [PseudoEMetricSpace T] [PseudoEMetricSpace X]
   isometry : ∀ s ∈ sets, ∃ f : T → X, Isometry f ∧ s = .range f
 
 /-- The vectors from one boundary vertex to the next (in counterclockwise order)
-of the tile Tile(a,b) defined in the paper. -/
+of the tile Tile(𝑎,𝑏) defined in the paper. -/
 noncomputable def vec (a b : NNReal) : Fin 13 → ℝ × ℝ :=
-  ![a • (2,0), a • (1,√3) / 2, b • (√3,-1) / 2, b • (√3,1) / 2, a • (-1,√3) / 2, a • (-1,0),
-    b • (0,1), b • (-√3,1) / 2, a • (-1,-√3) / 2, a • (-1,0), b • (0,-1), b • (-√3,-1) / 2,
-    a • (1,-√3) / 2]
+  ![a • (2,0), a • (1/2,√3/2), b • (√3/2,-1/2), b • (√3/2,1/2), a • (-1/2,√3/2), a • (-1,0),
+    b • (0,1), b • (-√3/2,1/2), a • (-1/2,-√3/2), a • (-1,0), b • (0,-1), b • (-√3/2,-1/2),
+    a • (1/2,-√3/2)]
 
 /-- The Euclidean plane. -/
 abbrev Plane := EuclideanSpace ℝ (Fin 2)
@@ -39,41 +39,43 @@ abbrev Plane := EuclideanSpace ℝ (Fin 2)
 /-- Construct a point in the Euclidean plane from a pair of real numbers. -/
 def toPlane (x : ℝ × ℝ) : Plane := .toLp (p := 2) ![x.1, x.2]
 
-/-- The boundary vertices (in counterclockwise order) of Tile(a,b). -/
+/-- The boundary vertices (in counterclockwise order) of Tile(𝑎,𝑏). -/
 noncomputable def vertex (a b : NNReal) (i : Fin 13) : Plane :=
   toPlane <| ∑ j ∈ Finset.Ioi i, vec a b j
 
-/-- The boundary polygon of Tile(a,b). -/
+/-- The boundary polygon of Tile(𝑎,𝑏). Notice that 12 + 1 = 0 in Fin 13. -/
 def polygon (a b : NNReal) : Set Plane :=
-  ⋃ i : ZMod 13, convexHull ℝ {vertex a b i, vertex a b (i + 1 : ZMod 13)}
+  ⋃ i : Fin 13, segment ℝ (vertex a b i) (vertex a b (i + 1))
 
-/-- The tile Tile(a,b), which is the closure of the bounded component of the complement of
-the boundary polygon. Notice that (a, √3/2*a) always lies in the bounded component. -/
+/-- The tile Tile(𝑎,𝑏), which is the closure of the bounded component of the complement of
+the boundary polygon. Notice that the open segment between the 2nd vertex (5𝑎/2, √3𝑎/2) and the
+6th vertex (𝑎+√3𝑏, √3𝑎) always lies in the bounded component, even when one of 𝑎 and 𝑏 is 0.
+The tile could also be decomposed into 6 smaller polygons, 5 of which are convex, see
+https://www.desmos.com/calculator/2tcx1o6uoy. -/
 def tile (a b : NNReal) : Set Plane :=
-  closure <| connectedComponentIn (polygon a b)ᶜ (.toLp (p := 2) ![a, √3/2*a])
+  closure <| connectedComponentIn (polygon a b)ᶜ (midpoint ℝ (vertex a b 2) (vertex a b 6))
 
-/-- A chiral tiling of the Euclidean plane by Tile(a,b) consists of tiles that are images
-of Tile(a,b) under orientation preserving isometries of the plane. -/
+/-- A chiral tiling of the Euclidean plane by Tile(𝑎,𝑏) consists of tiles that are images
+of Tile(𝑎,𝑏) under orientation preserving isometries of the plane. -/
 structure ChiralTilingBy (a b : NNReal) extends TopologicalTiling Plane where
   isometry : ∀ s ∈ sets, ∃ f : Plane →ᵃ[ℝ] Plane, Isometry f ∧ f.linear.det = 1 ∧ s = f '' tile a b
 
 /-- A collection of sets is aperiodic if it is not invariant under any nontrivial translation. -/
-def IsAperiodic {X : Type*} [AddZeroClass X] (S : Set (Set X)) : Prop :=
+def Aperiodic {X : Type*} [AddZeroClass X] (S : Set (Set X)) : Prop :=
   ∀ x : X, (Set.image (· + x)) '' S = S → x = 0
 
-/-- The main theorem of *An aperiodic monotile*: if a and b are distinct positive real numbers,
-then the Euclidean plane admits tilings by Tile(a,b), but only aperiodic ones. -/
+/-- The main theorem from *An aperiodic monotile*: if 𝑎 and 𝑏 are distinct positive real numbers,
+then the Euclidean plane admits tilings by Tile(𝑎,𝑏), but only aperiodic ones. -/
 @[eval_problem]
-theorem isAperiodic_tilingBy (a b : NNReal) (ha : a ≠ 0) (hb : b ≠ 0) (ne : a ≠ b) :
-    Nonempty (TilingBy (tile a b) Plane) ∧
-    ∀ t : TilingBy (tile a b) Plane, IsAperiodic t.sets := by
+theorem aperiodic_tilingBy (a b : NNReal) (ha : a ≠ 0) (hb : b ≠ 0) (ne : a ≠ b) :
+    Nonempty (TilingBy (tile a b) Plane) ∧ ∀ t : TilingBy (tile a b) Plane, Aperiodic t.sets := by
   sorry
 
-/-- One of the main theorem of *A chiral aperiodic monotile*: the Euclidean plane admits chiral
+/-- One of the main theorems from *A chiral aperiodic monotile*: the Euclidean plane admits chiral
 tilings by Tile(1,1), but only aperiodic ones. -/
 @[eval_problem]
-theorem isAperiodic_chiralTilingBy :
-    Nonempty (ChiralTilingBy 1 1) ∧ ∀ t : ChiralTilingBy 1 1, IsAperiodic t.sets := by
+theorem aperiodic_chiralTilingBy :
+    Nonempty (ChiralTilingBy 1 1) ∧ ∀ t : ChiralTilingBy 1 1, Aperiodic t.sets := by
   sorry
 
 end LeanEval.Geometry.AperiodicMonotiles
